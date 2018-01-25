2797f4c69c39d588c4c3d361b9c850f6ab431430
David Chen <davidchen@d-techpro.net>
2018-01-25 21:37:57 +0800

    updated

--- obd_capture.py
+++ obd_capture.py
@@ -28,22 +28,24 @@ class OBD_Capture():
 
         if(self.port):
             print "Connected to "+self.port.port.name
-            
+
     def is_connected(self):
         return self.port
-        
+
     def getSupportedSensorList(self):
-        return self.supportedSensorList 
+        return self.supportedSensorList
 
     def capture_data(self):
 
         text = ""
         #Find supported sensors - by getting PIDs from OBD
-        # its a string of binary 01010101010101 
+        # its a string of binary 01010101010101
         # 1 means the sensor is supported
         self.supp = self.port.sensor(0)[1]
         self.supportedSensorList = []
         self.unsupportedSensorList = []
+
+        print(self.supp)
 
         # loop through PIDs binary
         for i in range(0, len(self.supp)):
@@ -52,16 +54,16 @@ class OBD_Capture():
                 self.supportedSensorList.append([i+1, obd_sensors.SENSORS[i+1]])
             else:
                 self.unsupportedSensorList.append([i+1, obd_sensors.SENSORS[i+1]])
-        
+
         for supportedSensor in self.supportedSensorList:
             text += "supported sensor index = " + str(supportedSensor[0]) + " " + str(supportedSensor[1].shortname) + "\n"
-        
+
         time.sleep(3)
-        
+
         if(self.port is None):
             return None
 
-        #Loop until Ctrl C is pressed        
+        #Loop until Ctrl C is pressed
         localtime = datetime.now()
         current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)+"."+str(localtime.microsecond)
         #log_string = current_time + "\n"
--- show-serials.py
+++ show-serials.py
@@ -1,5 +1,6 @@
 from obd_capture import OBD_Capture
 
 cap = OBD_Capture()
-obddata = cap.capture_data
+cap.connect()
+obddata = cap.capture_data()
 print(obddata)
