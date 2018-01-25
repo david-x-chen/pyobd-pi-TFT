dd7c78e7aba223555a11fb31f0a28bc46a42a039
David Chen <davidchen@d-techpro.net>
2018-01-25 21:45:06 +0800

    updated

--- obd_capture.py
+++ obd_capture.py
@@ -74,6 +74,7 @@ class OBD_Capture():
             sensorIndex = supportedSensor[0]
             (name, value, unit) = self.port.sensor(sensorIndex)
             text += name + " = " + str(value) + " " + str(unit) + "\n"
+            print("values: %s %s %s" % (name, value, unit))
 
         return text
 
